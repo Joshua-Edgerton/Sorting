11d596aea5a5e992b2c9f73aaed06537bd8aac45
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-03-17 17:47:53 -0500

    Updated comments to be more clear and accurate

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,48 +4,52 @@ def merge( arrA, arrB ):
     merged_arr = [0] * elements
     # TO-DO
 
-    #Set values for a while loop
+    #Set values for a while loop.
+    #values are used to break while loops.
     a = b = c = 0
 
-    #While both arrays lengths are greater than 0 (to start)
+    #While both arrays lengths are greater than 0 (to start).
     while a < len(arrA) and b < len(arrB):
-        #If the first element in arrA is less than the first element in arrB
+        #If the first element in arrA is less than the first element in arrB.
         if (arrA[a] < arrB[b]):
-            #Then the first element in a new array will be equal to the first element in arrA
+            #Then the first element in a new array will be equal to the first element in arrA.
             merged_arr[c] = arrA[a]
-            #Add values to "a" and "c" in order to iterate through different indexes next time
+            #Add values to "a" and "c" in order to stop iteration.
             a += 1
             c += 1
-        #Otherwise (if the element in arrB was lower)
+        #Otherwise (if the element in arrB was lower).
         else:
-            #Make the first element in the new array equal to arrB's first element instead
+            #Make the first element in the new array equal to arrB's first element instead.
             merged_arr[c] = arrB[b]
-            #Update values in order to iterate through different indexes next time
+            #Update values in order to stop iteration.
             b += 1
             c += 1
-    #While the value of "a" (which is used to iterate through indexes) is less than the length of arrA
+    #While the value of "a" is less than the length of arrA.
     while a < len(arrA):
-        #The value of "c"(used to keep track of how many times youve added values to the new array) equals the value in arrA
+        #The value of "c" equals the value in arrA.
         merged_arr[c] = arrA[a]
         a += 1
         c += 1
-    #While the value of "b" (which is used to iterate through indexes) is less than the length of arrB
+    #While the value of "b" is less than the length of arrB.
     while b < len(arrB):
-        #The value of "c"(used to keep track of how many times youve added values to the new array) equals the value in arrB
+        #The value of "c" equals the value in arrB.
         merged_arr[c] = arrB[b]
         b += 1
         c += 1
     
     return merged_arr
 
-
-
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+
+#Function that runs first.
+def merge_sort(arr):
+    #If the length of the given array is greater than 1.
     if len(arr) > 1:
-        left = merge_sort(arr[0 : len(arr) // 2])
-        right = merge_sort(arr[len(arr) // 2 :])
+        #"left" = the first element in the array, to half way through the elements.
+        left = merge_sort(arr[0:len(arr) // 2])
+        #"right" = the last element.
+        right = merge_sort(arr[len(arr) // 2:])
+        #"arr" = the 2 elements (left and right) to be passed into the merge function above.
         arr = merge(left, right)
 
     return arr
